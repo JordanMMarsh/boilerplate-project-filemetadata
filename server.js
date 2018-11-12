0897b70147d1239852ffe73b6d8573637c3b8ddb
Jordan Marsh <39842679+JordanMMarsh@users.noreply.github.com>
2018-11-12 11:15:27 -0600

    Update server.js

--- server.js
+++ server.js
@@ -2,11 +2,17 @@
 
 var express = require('express');
 var cors = require('cors');
-
+const fs = require("fs");
+const bodyParser = require("body-parser");
+const path = require("path");
+const multer = require("multer");
+const upload = multer();
 // require and use "multer"...
 
 var app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -16,6 +22,10 @@ app.get('/', function (req, res) {
 
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
+});
+
+app.post('/api/fileanalyse', upload.any(), function(req, res) {
+  res.json({name: req.files[0].originalname, type: req.files[0].mimetype, size: req.files[0].size});
 });
 
 app.listen(process.env.PORT || 3000, function () {
